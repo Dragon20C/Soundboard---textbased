d0038b170f5d8e11a9de732d187ee5a7a2eb2893
Dragon20C <dragon212c@gmail.com>
2020-06-26 17:33:20 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,31 +1,22 @@
 import keyboard, simpleaudio as sa
 
-
 while True:
-    try:
-        user = int(input("Please pick a number: "))
-    except ValueError:
-        print("This isn't a number")
-
-    if user == 1:
+    if keyboard.is_pressed("1"):
         wave_obj = sa.WaveObject.from_wave_file("sound effects/creeperexplosion.wav")
         play_obj = wave_obj.play()
         play_obj.wait_done()
-        user = 0
-    if user == 2:
+
+    if keyboard.is_pressed("2"):
         wave_obj = sa.WaveObject.from_wave_file("sound effects/bruh.wav")
         play_obj = wave_obj.play()
         play_obj.wait_done()
-        user = 0
-    if user == 3:
+
+    if keyboard.is_pressed("3"):
         wave_obj = sa.WaveObject.from_wave_file("sound effects/alert.wav")
         play_obj = wave_obj.play()
         play_obj.wait_done()
-        user = 0
-    if user == 4:
+
+    if keyboard.is_pressed("4"):
         wave_obj = sa.WaveObject.from_wave_file("sound effects/fail.wav")
         play_obj = wave_obj.play()
         play_obj.wait_done()
-        user = 0
-    if keyboard.is_pressed("p"):
-        print("key p was pressed")
